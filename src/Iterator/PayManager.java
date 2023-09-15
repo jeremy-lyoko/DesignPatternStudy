77f44b34f9f79011a938d6bf4b88d68bbcda568d
De Ranger <71481878+jeremy-lyoko@users.noreply.github.com>
2023-09-15 19:09:53 +0800

    Update PayManager.java

--- src/Iterator/PayManager.java
+++ src/Iterator/PayManager.java
@@ -40,7 +40,7 @@ public class PayManager extends Aggregate {
     }
 
     @Override
-    public Iterator createIterator() {
+    public Iterator createIterator() { //继承抽象工厂类，返回接口实现类的对象
         return new CollectionIterator(this);
     }
 }
